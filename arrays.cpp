39ee670d76e590743b6086c56fa35254d730028d
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2023-11-27 17:15:34 -0300

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Declaring and initializing a 5 positions array with a integer type
+    int A[5] = {0,1,2,3,4};
+    for (int i = 0; i<5; i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    int syze = sizeof(A);
+    cout << syze << endl;
+    // 4 bytes (integer) * 5 (number of positions)
+    return 0;
+}
