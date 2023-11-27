76aa7f7afe711cc228e8e9d4d83c1739443d770e
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2023-11-27 20:08:43 -0300

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    //Declaring and initializing a 5 positions array with a integer type
+    // Declaring and initializing a 5 positions array with a integer type
     int A[5] = {0,1,2,3,4};
     for (int i = 0; i<5; i++){
         cout << A[i] << " ";
